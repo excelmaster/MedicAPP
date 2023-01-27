5268441f94201db1e7cc80040dcb6bd6f04e4330
excelmaster <betfie1@gmail.com>
2023-01-27 12:28:49 -0500

    create user and control duplicity 18-47

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const Event = require('./models/event');
-
+const User = require('./models/user');
 const app = express();
 
 app.use(bodyParser.json());
@@ -21,18 +22,34 @@ app.use(
           price: Float!
           date: String!
         }
+
+        type User {
+          _id: ID,
+          email: String!,
+          password: String
+        }
+
         input EventInput {
           title: String!
           description: String!
           price: Float!
           date: String!
         }
+
+        input UserInput {
+          email: String!
+          password: String!
+        }
+
         type RootQuery {
             events: [Event!]!
         }
+
         type RootMutation {
             createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
         }
+
         schema {
             query: RootQuery
             mutation: RootMutation
@@ -67,6 +84,28 @@ app.use(
             console.log(err);
             throw err;
           });
+      },
+      createUser: args => {
+        return User.findOne({email: args.userInput.email})
+          .then(user => {
+            if(user){
+              throw new Error('User already exists');
+            }
+            return bcrypt.hash(args.userInput.password, 12)
+          })                  
+          .then(hashedPassword => {
+            const user = new User({
+              email: args.userInput.email,
+              password: hashedPassword
+            })
+            return user.save();            
+          })
+            .then( result => {
+              return {...result._doc, _id: result._id}
+            })
+            .catch(err => {
+              throw err;
+            })
       }
     },
     graphiql: true
--- models/user.js
+++ models/user.js
@@ -14,7 +14,6 @@ const userSchema = new Schema({
         type: Schema.Types.ObjectId,        
         ref: 'Event'
     }]
-
 });
 
 module.exports = mongoose.model('User', userSchema);
