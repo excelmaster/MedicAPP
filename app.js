04a0c11d42d33b2ff1fe3b39b8a28288a991dba4
excelmaster <betfie1@gmail.com>
2023-01-26 19:24:17 -0500

    save and retrieve events, save and validate duplicates in users

---
+++ app.js
@@ -0,0 +1,129 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {graphqlHTTP} = require('express-graphql');
+const {buildSchema} = require('graphql');
+const mongoose = require('mongoose');
+const Event = require('./models/event');
+const User = require('./models/user');
+const bcrypt = require('bcryptjs');
+const app = express();
+
+app.use(bodyParser.json());
+app.use('/graphql/', graphqlHTTP({
+    schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!            
+        }
+
+        type User {
+            _id:ID!
+            email: String!
+            password: String
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!            
+        }
+
+        input UserInput {
+            email: String!
+            password: String!
+        }
+
+        type RootQuery{
+            events: [Event!]!
+            users: [User!]!
+        }
+
+        type RootMutation{
+            createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }    
+    `),
+    rootValue: {
+        events: () => {
+            return Event.find()
+                .then(events => {
+                    return events.map(event => {
+                        return {
+                            ...event._doc
+                        };
+                    });
+                })
+                .catch(err => {
+                    throw err;
+                })
+        },
+        createEvent: (args) => {
+            const event = new Event({
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: new Date(args.eventInput.date),
+                creator: '63d3165135f89d6becd94f2d'
+            });
+            return event
+                .save()
+                .then(result => {
+                    console.log(result);
+                    return {
+                        ...result._doc
+                    };
+                })
+                .catch(err => {
+                    console.log(err);
+                    throw err;
+                });
+        },
+        createUser: (args) => {
+            return User.findOne({email: args.userInput.email})
+                .then(user => {
+                    if(user) {
+                        throw new Error("user already exists");
+                    }
+                    return bcrypt
+                    .hash(args.userInput.password,12);
+                })               
+                .then(hashedPassword => {
+                    const user = new User({
+                        email: args.userInput.email,
+                        password: hashedPassword
+                    });
+                    return user.save();
+                })
+                .then(result => {
+                    return {...result._doc, _id: result.id.Date.toString() 
+                    };
+                })
+                .catch(err => {
+                    throw err;
+                });        
+            }
+        },    
+        graphiql: true,
+    })
+);
+
+app.get('/', (req, res, next) => {
+        res.send("hola perros")
+    })
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWD}@cluster0.e9jdyl5.mongodb.net/${process.env.MONGO_DB}`)
+    .then(() => {
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: 'string',
+        required: true
+    },
+    password: {
+        type: 'string',
+        required: true
+    },
+    createdEvents: [{
+        type: Schema.Types.ObjectId,        
+        ref: 'Event'
+    }]
+
+});
+
+module.exports = mongoose.model('User', userSchema);
